a790d35b549e69138d7f8f425810fbcc6394837b
German Ariel Martinez <germartinez@itba.edu.ar>
2022-12-02 17:13:16 -0300

    GlobieHomePage done and postCard finished

---
+++ src/components/postCard.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="post">
+        <div class="pic">
+            <img class="pic-img" src="../assets/explore.jpg" alt="">
+            <button class="btn" type="button">View Post</button> 
+        </div>
+        <div class="data">
+            <div class="box1"><b-icon icon="geo-alt" style="color: white;margin-right:5px;"></b-icon> {{this.location}}</div>
+            <div class="box2"><b-icon icon="volume-up" style="color: white;margin-right:5px;"></b-icon> {{this.languages}}</div>
+            <div class="box3">{{this.rating}}<b-icon icon="star-fill" style="color: gold;margin-left:5px;padding-bottom:5px"></b-icon></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.box1 {
+    grid-column-start: 1;
+    grid-column-end: 1;
+    grid-row-start: 1;
+    grid-row-end: 1;
+}
+.box2 {
+    grid-column-start: 1;
+    grid-column-end: 1;
+    grid-row-start: 2;
+    grid-row-end: 2;
+}
+.box3 {
+    grid-column-start: 2;
+    grid-column-end: 2;
+    grid-row-start: 1;
+    grid-row-end: 3;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    font-size: 2em;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+}
+.btn {
+    position: absolute;
+    top: 5%;
+    right: 2.5%;
+    border: none;
+    outline: none;
+    height: 40px;
+    color: white;
+    background-color: #c62034;
+    font-size: 18px;
+    border-radius: 20px;
+    transition: all 1s ease;
+}
+.btn:hover {
+    background-color: white;
+    transition: all 1s ease;
+    color: #c62034;
+}
+.pic-img {
+    float: left;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.pic {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 70%;
+}
+.data {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    align-items: center;
+    justify-items: flex-start;
+    color: white;
+    width: 95%;
+    height: 30%;
+}
+.post {
+    position: relative;
+    background-color: #202844;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 3vh;
+    margin-bottom: 3vh;
+    width: 35vw; 
+    height: 35vh;
+}
+</style>
+
+<script>
+export default {
+    props: {
+        location: String,
+        languages: String,
+        rating: Number
+        // pid: Number
+    }
+}
+</script>
--- src/views/GlobieLandingPage.vue
+++ src/views/GlobieLandingPage.vue
@@ -2,20 +2,139 @@
     <div class="postsDisplay">
         <!-- Panel de filtros -->
         <div class="filterMenu">
-            <ul class="filters">
-                <li></li>
-                <li></li>
-                <li></li>
+            <div class="filter_section_title">
+                <span class="title">Here you can filter posts</span>
+            </div> 
+            <ul class="buttons">
+                <li><v-select label="Order by" outlined></v-select></li>
+                <li><v-select label="Show ratings" outlined></v-select></li>
+                <li><v-select label="Select country" outlined></v-select></li>
             </ul>
+            <!-- Lenguajes checkboxes -->
+            <div class="filter_section_title">
+                <span class="title">Languages</span>
+            </div>
+            <div class="languages">
+                <ul class="checkboxes" style="margin-left: 10%">
+                    <li><v-checkbox v-model="checkbox" label="Spanish"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="German"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="Italian"></v-checkbox></li>
+                </ul>
+                <ul class="checkboxes">
+                    <li><v-checkbox v-model="checkbox" label="Japanese"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="Korean"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="Portuguese"></v-checkbox></li>
+                </ul>
+            </div>
+            <!-- Caracteristicas de los departamentes -->
+            <div class="filter_section_title">
+                <span class="title">Post features</span>
+            </div>
+            <div class="languages">
+                <ul class="checkboxes" style="margin-left: 10%">
+                    <li><v-checkbox v-model="checkbox" label="Pool"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="Allow pets"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="Gym"></v-checkbox></li>
+                </ul>
+                <ul class="checkboxes">
+                    <li><v-checkbox v-model="checkbox" label="Yard"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="TV"></v-checkbox></li>
+                    <li><v-checkbox v-model="checkbox" label="Wifi"></v-checkbox></li>
+                </ul>
+            </div>       
         </div>
         <!-- Posts -->
         <div class="postsViewer">
-            h
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
+            <card location="Australia, Victoria" languages="French, German, Portuguese, Dutch" rating="5"/>
         </div>
     </div>
 </template>
 
 <style scoped>
+    .postsViewer {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        align-items: center;
+        justify-items: center;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(39,42,53,0.3);
+        overflow-y: auto;
+    }
+    .languages {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    .checkboxes {
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        width: 90%;
+        justify-content: flex-start;
+        align-items: flex-start;
+        margin:0;
+        padding: 0;
+    }
+    .checkboxes li {
+        list-style-type: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        margin:0;
+        padding: 0;
+    }
+    .title {
+        border-top-style: solid;
+        border-bottom-style: solid;
+        border-width: .15vh;
+        border-color:white;
+        height: 90%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        font-size: 2vh;
+    }
+    .filter_section_title {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        background-color: #202844;
+        color: white;
+        height: 4vh;
+        margin: 0;
+        padding: 0;
+    }
+    .buttons {
+        width: 100%;
+        margin-top: 2.5vh;
+    }
+    .buttons li {
+        list-style-type: none;
+        display: flex;
+        flex-direction: row;
+        height: 10vh;
+        width: 90%;
+        justify-content: center;
+        align-items: center;
+        margin:0;
+        padding: 0;
+    }
+    .buttons li:first-child {
+        margin-top:6vh;
+    }
     .postsDisplay {
         display: grid;
         height: 100%;
@@ -24,26 +143,22 @@
         grid-template-columns: 1.125fr 4fr;
     }
     .filterMenu {
-        display: grid;
         height: 100%;
         width: 100%;
-        background-color: purple;
+        margin:0;
+        padding: 0;
+        overflow-y: auto;
     }
-    .postsViewer {
-        display: grid;
-        width: 100%;
-        height: 100%;
-        background-color: greenyellow;
+    .filterMenu::-webkit-scrollbar{
+        display: none;
     }
 </style>
 
 <script>
+import card from '@/components/postCard.vue'
 export default {
-    name: 'HomePage',
-    methods: {
-        sotm(){
-            this.$router.push('/home/sotm')
-        }
+    components: {
+        card,
     }
 }
 </script>
